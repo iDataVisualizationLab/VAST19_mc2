f10bd536d68ffdd608b4a8ce3ee3caf09e7a9d62
gj0706 <32847054+gj0706@users.noreply.github.com>
2019-07-03 23:21:32 -0500

    updated file reading

--- js/map.js
+++ js/map.js
@@ -1,30 +1,41 @@
 var filelist = [];
 for ( i = 1; i < 20; i ++ )
 {
-    var filename = "data/aggDataHeatmap/Region" + i + ".csv";
-    // var filename = "data/aggTimeByReg/region" + i + ".csv";
+    // var filename = "data/aggDataHeatmap/Region" + i + ".csv";
+    var filename = "data/aggTimeByReg/region" + i + ".csv";
 
     filelist.push(d3.csv(filename));
 }
 
-var tsfilelist = [];
-for ( i = 1; i < 20; i ++ )
-{
-    var tsfilename = "data/AggRegid/Region" + i + ".csv";
-    tsfilelist.push(d3.csv(tsfilename));
-}
+// var tsfilelist = [];
+// for ( i = 1; i < 20; i ++ )
+// {
+//     var tsfilename = "data/AggRegid/Region" + i + ".csv";
+//     tsfilelist.push(d3.csv(tsfilename));
+// }
 
-var mbfilelist = [];
-for (i = 1; i < 51; i++)
-{
-    var mbfilename = "data/mobileMaxValue/mobile" + i + ".csv";
-    mbfilelist.push(d3.csv(mbfilename));
-}
-console.log(mbfilelist);
+// var mbfilelist = [];
+// for (i = 1; i < 51; i++)
+// {
+//     var mbfilename = "data/mobileMaxValue/mobile" + i + ".csv";
+//     mbfilelist.push(d3.csv(mbfilename));
+// }
+// console.log(mbfilelist);
+
+// load data for time series
+d3.csv("data/allSensorReadings.csv").then(data=>{
+    data.forEach(d=>{
+        d.Timestamp = parse(d.Timestamp);
+        d.Value = +d.Value;
+    })
+    // drawTimeSeries(data);
+
+
+
 
 Promise.all(filelist).then(files => {
-    Promise.all( tsfilelist ).then( tsfiles => {
-        Promise.all(mbfilelist).then( mbfiles=> {
+    // Promise.all( tsfilelist ).then( tsfiles => {
+        // Promise.all(mbfilelist).then( mbfiles=> {
             var index = 1;
             var alldata = [];
             for (let i = 0; i < files.length; i++) {
@@ -34,28 +45,28 @@ Promise.all(filelist).then(files => {
                 })
                 alldata.push(files[i]);
             }
-            var mbdata =[];
-            for (let j = 0; j < mbfiles.length; j++) {
-                mbfiles[j].forEach(f => {
-                    f.Timestamp = parse(f.Timestamp);
-                    f.Value = +f.Value;
-                    f.Long = +f.Long;
-                    f.Lat = +f.Lat;
-                })
-                mbdata.push(mbfiles[j]);
-            }
-            console.log(mbdata[0]);
+            // var mbdata =[];
+            // for (let j = 0; j < mbfiles.length; j++) {
+            //     mbfiles[j].forEach(f => {
+            //         f.Timestamp = parse(f.Timestamp);
+            //         f.Value = +f.Value;
+            //         f.Long = +f.Long;
+            //         f.Lat = +f.Lat;
+            //     })
+            //     mbdata.push(mbfiles[j]);
+            // }
+            // console.log(mbdata[0]);
 
-            const heatmapIds = [];
-            for (let i = 1; i < 20; i++) {
-                heatmapIds.push("region" + i + "heatmap");
-            }
-            console.log(heatmapIds);
+            // const heatmapIds = [];
+            // for (let i = 1; i < 20; i++) {
+            //     heatmapIds.push("region" + i + "heatmap");
+            // }
+            // console.log(heatmapIds);
 
 
             //draw initial heatmap and timeseries
-            draw_heatmap(alldata[index - 1]);
-            drawTimeSeries(tsfiles[index - 1]);
+            draw_heatmap(alldata[index - 1],1);
+            // drawTimeSeries(tsfiles[index - 1]);
             // draw_mobile_location(mddata[index -1]);
 
             const mapTip = d3.select("#map")
@@ -133,15 +144,21 @@ Promise.all(filelist).then(files => {
                         .on("mouseleave", mouseleave)
                         .on("click", d => {
                             // d3.selectAll("path").classed("unselected",true);
-                            d3.select("#"+"d.id").classed("selected",true);
+                            // d3.select("#"+"d.id").classed("selected",true);
+
+                            // toggleHeatmap("heatmap" + (index + 1));
                             for (const region of regionNameList) {
                                 const index = regionNameList.indexOf(region);
+
+
                                 if (d.properties.Nbrhood === region) {
-                                    draw_heatmap(alldata[index]);
-                                    drawTimeSeries(tsfiles[index]);
+                                    draw_heatmap(alldata[index],index+1);
+                                    // drawTimeSeries(tsfiles[index]);
                                 }
                             }
-                            // toggleHeatmap();
+
+
+
                         })
 
                     //add region names to map
@@ -185,41 +202,41 @@ Promise.all(filelist).then(files => {
 
 
 
-                    //plot initial mobile locations
-                    const dotTip = d3.select("#map")
-                        .append("div")
-                        .style("opacity", 0)
-                        .attr("class", "tstooltip");
-
-                    mapSvg
-                        .data(mbdata[0])// set mobile sensor 1 as initial data
-                        .enter()
-                        .append("circle")
-                        .attr("class","dots")
-                        .attr("cx", d=> {
-                            return projection([d.Long, d.Lat])[0];
-                        })
-                        .attr("cy", function(d) {
-                            return projection([d.Long, d.Lat])[1];
-                        })
-                        .attr("r", "3")
-                        .style("fill", "#31a354")
-                        .style("opacity",.8)
-                        .on("mouseover", d=>{
-                            dotTip.transition()
-                                .duration(200)
-                                .style("opacity","1");
-                            dotTip
-                                .html("Time: " + d.Timestamp.toLocaleTimeString([], { year: '2-digit', month: '2-digit',day: '2-digit', hour: '2-digit', minute:'2-digit'})  + "<br>"
-                                    + "Value  : " + d.Value.toFixed(2) + " (cmp)")
-                                // .style("left", (d3.mouse(this)[0] + 0) + "px")
-                                // .style("top", (d3.mouse(this)[1]) + 0 + "px");})
-                                .style("left", d3.select(this).attr("cx") + "px")
-                                .style("top", d3.select(this).attr("cy") + "px");})
-                        .on("mouseout",()=>{
-                            dotTip.transition()
-                                .duration(200)
-                                .style("opacity","0");});
+                    // //plot initial mobile locations
+                    // const dotTip = d3.select("#map")
+                    //     .append("div")
+                    //     .style("opacity", 0)
+                    //     .attr("class", "tstooltip");
+                    //
+                    // mapSvg
+                    //     .data(mbdata[0])// set mobile sensor 1 as initial data
+                    //     .enter()
+                    //     .append("circle")
+                    //     .attr("class","dots")
+                    //     .attr("cx", d=> {
+                    //         return projection([d.Long, d.Lat])[0];
+                    //     })
+                    //     .attr("cy", function(d) {
+                    //         return projection([d.Long, d.Lat])[1];
+                    //     })
+                    //     .attr("r", "3")
+                    //     .style("fill", "#31a354")
+                    //     .style("opacity",.8)
+                    //     .on("mouseover", d=>{
+                    //         dotTip.transition()
+                    //             .duration(200)
+                    //             .style("opacity","1");
+                    //         dotTip
+                    //             .html("Time: " + d.Timestamp.toLocaleTimeString([], { year: '2-digit', month: '2-digit',day: '2-digit', hour: '2-digit', minute:'2-digit'})  + "<br>"
+                    //                 + "Value  : " + d.Value.toFixed(2) + " (cmp)")
+                    //             // .style("left", (d3.mouse(this)[0] + 0) + "px")
+                    //             // .style("top", (d3.mouse(this)[1]) + 0 + "px");})
+                    //             .style("left", d3.select(this).attr("cx") + "px")
+                    //             .style("top", d3.select(this).attr("cy") + "px");})
+                    //     .on("mouseout",()=>{
+                    //         dotTip.transition()
+                    //             .duration(200)
+                    //             .style("opacity","0");});
 
 
 
@@ -287,15 +304,24 @@ Promise.all(filelist).then(files => {
 
             // where helper functions go
 
-            function toggleHeatmap(){
-                let blocks = d3.select(".heatmapBlock");
-                for(let i=0; i < blocks.length; i++ ){
-                    if(blocks[i].style.display == "block"){
-                        blocks[i].style.display = "none";
-                        return;
-                    }
-                    blocks[i].style.display = "block";
+            function toggleHeatmap(id){
+                const selected = document.getElementById(id);
+                if(!selected) return true;
+                if(selected.style.display == "none"){
+                    selected.style.display = "block";
+                }else{
+                    selected.style.display = "none";
                 }
+              return true;
+                // let selected = document.getElementById("")
+                // let blocks = d3.select(".heatmapBlock");
+                // for(let i=0; i < blocks.length; i++ ){
+                //     if(blocks[i].style.display == "block"){
+                //         blocks[i].style.display = "none";
+                //         return;
+                //     }
+                //     blocks[i].style.display = "block";
+                // }
 
             }
 
@@ -332,6 +358,8 @@ Promise.all(filelist).then(files => {
                     .style("opacity", 1)
             }
 
-        });
-    });
+        // });
+    // });
+});
+
 });
